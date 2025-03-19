8eff50ef479e90a63706acab76139e8c5480fc2b
Nagul75 <nagulk697@gmail.com>
2025-03-19 14:16:38 +0530

    Set up winston logger

--- logger/logger.js
+++ logger/logger.js
@@ -1,1 +1,18 @@
-console.log(idk)
+const winston = require('winston')
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({timestamp, level, message}) => {
+            return `${timestamp} [${level}]: ${message}`
+        })
+    ),
+    transports: [
+        new winston.transports.Console({format: winston.format.simple()}),
+        new winston.transports.File({filename: 'combined.log'}),
+        new winston.transports.File({filename: 'error.log', level: 'error'})
+    ]
+})
+
+module.exports = logger
